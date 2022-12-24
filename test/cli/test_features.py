d06cbf41c35b3694d731cab87a34745b241c98d1
Daniel Braun <daniel.braun@outlook.co.il>
2022-12-24 21:25:50 +0000

    test: enrich test with npm package and pipx package dependencies

--- test/cli/test_features.py
+++ test/cli/test_features.py
@@ -2,15 +2,40 @@ import os
 from devcontainer_contrib.cli.features import generate, OutputType
 from helpers import RESOURCE_DIR
 import pathlib
+import pytest
 
-ACT_FEATURE_DEFINITION = os.path.join(RESOURCE_DIR, "alp-asdf-feature-definition.json")
-FEATURE_ID = "alp-asdf"
+FEATURE_DEFENITIONS = {
+    "alp-asdf": os.path.join(
+        RESOURCE_DIR, "test_feature_definitions", "alp-asdf", "feature-definition.json"
+    ),
+    "angular-cli": os.path.join(
+        RESOURCE_DIR,
+        "test_feature_definitions",
+        "angular-cli",
+        "feature-definition.json",
+    ),
+    "ansible": os.path.join(
+        RESOURCE_DIR, "test_feature_definitions", "ansible", "feature-definition.json"
+    ),
+    "localstack": os.path.join(
+        RESOURCE_DIR,
+        "test_feature_definitions",
+        "localstack",
+        "feature-definition.json",
+    ),
+}
+
 TEST_IMAGE = "mcr.microsoft.com/devcontainers/base:debian"
 
 
-def test_features_generate_dependencies(tmp_path: str) -> None:
+@pytest.mark.parametrize(
+    "feature_id,feature_definition", [(k, v) for k, v in FEATURE_DEFENITIONS.items()]
+)
+def test_features_generate_dependencies(
+    tmp_path: str, feature_id: str, feature_definition: str
+) -> None:
     generate(
-        feature_definition=ACT_FEATURE_DEFINITION,
+        feature_definition=feature_definition,
         output_dir=tmp_path,
         output_type=OutputType.dependencies,
     )
@@ -18,9 +43,14 @@ def test_features_generate_dependencies(tmp_path: str) -> None:
     assert os.path.isfile(os.path.join(tmp_path, "dependencies.sh"))
 
 
-def test_feature_generate_test(tmp_path: str) -> None:
+@pytest.mark.parametrize(
+    "feature_id,feature_definition", [(k, v) for k, v in FEATURE_DEFENITIONS.items()]
+)
+def test_feature_generate_test(
+    tmp_path: str, feature_id: str, feature_definition: str
+) -> None:
     generate(
-        feature_definition=ACT_FEATURE_DEFINITION,
+        feature_definition=feature_definition,
         output_dir=tmp_path,
         output_type=OutputType.test,
     )
@@ -28,27 +58,32 @@ def test_feature_generate_test(tmp_path: str) -> None:
     assert os.path.isfile(os.path.join(tmp_path, "test.sh"))
 
 
-def test_feature_generate_feature_dir(shell, tmp_path: pathlib.Path) -> None:
+@pytest.mark.parametrize(
+    "feature_id,feature_definition", [(k, v) for k, v in FEATURE_DEFENITIONS.items()]
+)
+def test_feature_generate_feature_dir(
+    shell, tmp_path: pathlib.Path, feature_id: str, feature_definition: str
+) -> None:
     tmp_path_str = tmp_path.as_posix()
     generate(
-        feature_definition=ACT_FEATURE_DEFINITION,
+        feature_definition=feature_definition,
         output_dir=tmp_path,
         output_type=OutputType.feature_dir,
     )
 
-    assert os.path.isfile(os.path.join(tmp_path_str, "test", FEATURE_ID, "test.sh"))
+    assert os.path.isfile(os.path.join(tmp_path_str, "test", feature_id, "test.sh"))
     assert os.path.isfile(
-        os.path.join(tmp_path_str, "src", FEATURE_ID, "dependencies.sh")
+        os.path.join(tmp_path_str, "src", feature_id, "dependencies.sh")
     )
     assert os.path.isfile(
-        os.path.join(tmp_path_str, "src", FEATURE_ID, "devcontainer-feature.json")
+        os.path.join(tmp_path_str, "src", feature_id, "devcontainer-feature.json")
     )
-    assert os.path.isfile(os.path.join(tmp_path_str, "src", FEATURE_ID, "install.sh"))
+    assert os.path.isfile(os.path.join(tmp_path_str, "src", feature_id, "install.sh"))
     assert os.path.isfile(
-        os.path.join(tmp_path_str, "src", FEATURE_ID, "install_command.sh")
+        os.path.join(tmp_path_str, "src", feature_id, "install_command.sh")
     )
     response = shell.run(
-        f"devcontainer features test -p {tmp_path_str} -f {FEATURE_ID}  -i {TEST_IMAGE}",
+        f"devcontainer features test -p {tmp_path_str} -f {feature_id}  -i {TEST_IMAGE}",
         shell=True,
     )
     print(response.stdout)
