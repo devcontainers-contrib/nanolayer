e1cc06c88dcec96f24c88a236852a4bed94f0672
Daniel Braun <daniel.braun@outlook.co.il>
2023-03-12 19:32:20 +0000

    feat: added --version and --release-version options. also dynamic resolving of package options

--- dcontainer/__main__.py
+++ dcontainer/__main__.py
@@ -1,9 +1,30 @@
 import typer
 
 from dcontainer.cli.feature import app as feature_app
+from dcontainer.utils.version import resolve_own_package_version, resolve_own_release_version
 
 app = typer.Typer(pretty_exceptions_show_locals=False, pretty_exceptions_short=False)
 app.add_typer(feature_app, name="feature")
+
+
+def version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"dcontainer version: {resolve_own_package_version()}")
+        raise typer.Exit()
+
+
+def release_version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"dcontainer release version: {resolve_own_release_version()}")
+        raise typer.Exit()
+
+@app.callback()
+def version(
+    version: bool = typer.Option(None, "--version", callback=version_callback, is_eager=True),
+    release_version: bool = typer.Option(None, "--release-version", callback=release_version_callback, is_eager=True),
+
+):
+    return 
 
 
 def main() -> None:
---
+++ dcontainer/utils/version.py
@@ -0,0 +1,37 @@
+import urllib
+import urllib.request
+import json
+from importlib.metadata import version
+from typing import Optional, List
+
+OWN_REPO = "devcontainers-contrib/cli"
+OWN_PACKAGE = "dcontainer"
+
+
+def _resolve_package_version(package: str) -> str:
+    return version(package)
+
+
+def _get_latest_release(repo: str) -> str:
+    response = urllib.request.urlopen(f"https://api.github.com/repos/{repo}/releases/latest")  # nosec
+    response_json = json.loads(response.read())
+    resolved_version = response_json['name']
+    return resolved_version
+
+
+def _get_github_tags(repo: str) -> List[str]:
+    response = urllib.request.urlopen(f"https://api.github.com/repos/{repo}/tags")  # nosec
+    return [tag['name'] for tag in json.loads(response.read())]
+
+
+def resolve_own_package_version() -> str:
+    return _resolve_package_version(OWN_PACKAGE)
+
+def resolve_own_release_version() -> str:
+    package_version = _resolve_package_version(OWN_PACKAGE)
+    tags = _get_github_tags(OWN_REPO)
+    if package_version is not None:
+        if f"v{package_version}" in tags:
+            return f"v{package_version}"
+    
+    return _get_latest_release(OWN_REPO)
