56de12ebdf493bef95b6a65729d109d856be8c67
Daniel Braun <daniel.braun@outlook.co.il>
2023-03-23 21:07:21 +0000

    ci: add docker platform argument

--- tests/gh_release/test_gh_release_installer.py
+++ tests/gh_release/test_gh_release_installer.py
@@ -6,7 +6,7 @@ from helpers import execute_current_python_in_container
 
 # @pytest.mark.skip(reason="not implemented yet")
 @pytest.mark.parametrize(
-    "test_command,excpected_result,image,repo,target",
+    "test_command,excpected_result,image,repo,target,docker_platform",
     [
         (
             "doctl version",
@@ -14,6 +14,7 @@ from helpers import execute_current_python_in_container
             "mcr.microsoft.com/devcontainers/base:debian",
             "digitalocean/doctl",
             "doctl",
+            "linux/amd64"
         ),
         (
             "argocd --help",
@@ -21,6 +22,7 @@ from helpers import execute_current_python_in_container
             "mcr.microsoft.com/devcontainers/base:debian",
             "argoproj/argo-cd",
             "argocd",
+            "linux/amd64"
         ),
         (
             "which kubectx",
@@ -28,6 +30,7 @@ from helpers import execute_current_python_in_container
             "mcr.microsoft.com/devcontainers/base:debian",
             "ahmetb/kubectx",
             "kubectx",
+            "linux/amd64"
         ),
         (
             "which kubens",
@@ -35,6 +38,7 @@ from helpers import execute_current_python_in_container
             "mcr.microsoft.com/devcontainers/base:debian",
             "ahmetb/kubectx",
             "kubens",
+            "linux/arm64"
         ),
         (
             "terrascan version",
@@ -42,6 +46,7 @@ from helpers import execute_current_python_in_container
             "mcr.microsoft.com/devcontainers/base:debian",
             "tenable/terrascan",
             "terrascan",
+            "linux/amd64"
         ),
         (
             "gh --version",
@@ -49,6 +54,7 @@ from helpers import execute_current_python_in_container
             "mcr.microsoft.com/devcontainers/base:debian",
             "cli/cli",
             "gh",
+            "linux/amd64"
         ),
         (
             "pwsh --version",
@@ -56,6 +62,7 @@ from helpers import execute_current_python_in_container
             "mcr.microsoft.com/devcontainers/base:debian",
             "PowerShell/PowerShell",
             "pwsh",
+            "linux/arm64"
         ),
     ],
 )
@@ -65,95 +72,12 @@ def test_gh_release_install(
     image: str,
     repo: List[str],
     target: str,
+    docker_platform: str,
 ) -> None:
     full_test_command = f"sudo PYTHONPATH=$PYTHONPATH python3 -m dcontainer install gh-release {repo} {target} && {test_command}"
 
     assert excpected_result == execute_current_python_in_container(
         test_command=full_test_command,
         image=image,
-    )
-
-
-@pytest.mark.parametrize(
-    "test_command,excpected_result,image,repo,target,version,asset_regex",
-    [
-        (
-            "doctl version",
-            0,
-            "mcr.microsoft.com/devcontainers/base:debian",
-            "digitalocean/doctl",
-            "doctl",
-            "1.93.1",
-            "doctl-1.93.1-linux-amd64.tar.gz",
-        ),
-        (
-            "argocd --help",
-            0,
-            "mcr.microsoft.com/devcontainers/base:debian",
-            "argoproj/argo-cd",
-            "argocd",
-            "2.6.6",
-            "argocd-linux-amd64",
-        ),
-        (
-            "which kubectx",
-            0,
-            "mcr.microsoft.com/devcontainers/base:debian",
-            "ahmetb/kubectx",
-            "kubectx",
-            "0.9.4",
-            "kubectx_v0.9.4_linux_x86_64.tar.gz",
-        ),
-        (
-            "which kubens",
-            0,
-            "mcr.microsoft.com/devcontainers/base:debian",
-            "ahmetb/kubectx",
-            "kubens",
-            "v0.9.4",
-            "kubens_v0.9.4_linux_x86_64.tar.gz",
-        ),
-        (
-            "terrascan version",
-            0,
-            "mcr.microsoft.com/devcontainers/base:debian",
-            "tenable/terrascan",
-            "terrascan",
-            "1.18.0",
-            "terrascan_1.18.0_Linux_x86_64.tar.gz",
-        ),
-        (
-            "gh --version",
-            0,
-            "mcr.microsoft.com/devcontainers/base:debian",
-            "cli/cli",
-            "gh",
-            "2.24.3",
-            "gh_2.24.3_linux_amd64.tar.gz",
-        ),
-        (
-            "pwsh --version",
-            0,
-            "mcr.microsoft.com/devcontainers/base:debian",
-            "PowerShell/PowerShell",
-            "pwsh",
-            "7.3.3",
-            "powershell-7.3.3-linux-x64.tar.gz",
-        ),
-    ],
-)
-def test_gh_release_install_with_regex(
-    test_command,
-    excpected_result: int,
-    image: str,
-    repo: List[str],
-    target: str,
-    version: str,
-    asset_regex: str,
-) -> None:
-    full_test_command = f"sudo PYTHONPATH=$PYTHONPATH python3 -m dcontainer install gh-release {repo} {target} --version {version} --asset-regex {asset_regex} && {test_command}"
-
-    assert excpected_result == execute_current_python_in_container(
-        test_command=full_test_command,
-        image=image,
+        docker_platform=docker_platform
     )
