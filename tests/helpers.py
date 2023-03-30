d41fb58de9879727b15746fbe6a48fa2f34d8e9f
Daniel Braun <daniel.braun@outlook.co.il>
2023-03-30 17:44:57 +0000

    removed devcontainer generation code

--- tests/helpers.py
+++ tests/helpers.py
@@ -9,14 +9,14 @@ from typing import Optional
 import git
 import invoke
 
-from minilayer.devcontainer.feature_generation.oci_feature_generator import (
+from dcontainer.devcontainer.feature_generation.oci_feature_generator import (
     OCIFeatureGenerator,
 )
-from minilayer.devcontainer.models.devcontainer_feature_definition import (
+from dcontainer.devcontainer.models.devcontainer_feature_definition import (
     FeatureDefinition,
     TestScenario,
 )
-from minilayer.installers.devcontainer_feature.models.devcontainer_feature import Mount
+from dcontainer.devcontainer.models.devcontainer_feature import Mount
 
 
 def execute_current_python_in_container(
