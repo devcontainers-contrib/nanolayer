a859d462d7629e02aafa9812fc9222fd583663d4
Daniel Braun <daniel.braun@outlook.co.il>
2023-04-12 21:44:20 +0000

    fix: better traces in binary mode

--- nanolayer/__main__.py
+++ nanolayer/__main__.py
@@ -1,5 +1,5 @@
 import typer
-
+import os
 from nanolayer.cli.install import app as install_app
 from nanolayer.utils.analytics import setup_analytics
 from nanolayer.utils.settings import NanolayerSettings
@@ -7,6 +7,11 @@ from nanolayer.utils.version import (
     resolve_own_package_version,
     resolve_own_release_version,
 )
+
+if "__file__" not in globals():
+    # typer exception handling using __file__ which is
+    # not available when running in pyoxidizer binary mode
+    os.environ["_TYPER_STANDARD_TRACEBACK"] = "1"
 
 app = typer.Typer(pretty_exceptions_show_locals=False, pretty_exceptions_short=False)
 app.add_typer(install_app, name="install")
