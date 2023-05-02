761ff99dc0ef12bdabf61b2fec9d9a8f680c5671
Daniel Braun <daniel.braun@outlook.co.il>
2023-05-02 08:12:00 +0000

    feat: more checksum filters

--- nanolayer/installers/gh_release/resolvers/asset_resolver.py
+++ nanolayer/installers/gh_release/resolvers/asset_resolver.py
@@ -63,7 +63,7 @@ class AssetResolver:
 
     MISC_REGEX_MAP = {
         "packages": r"(\.deb|\.rpm|\.pkg|\.apk|\.[Aa]ppImage|\.snap)",
-        "checksums": r"(\.sig$|\.text$|\.txt$|[Cc]hecksums|sha256)",
+        "checksums": r"(\.sig$|\.text$|\.txt$|[Cc]hecksums|sha256|\.sha1$|\.md5$)",
         "certificates": r"(\.pub$|\.pem$|\.crt$|\.asc$|pivkey|pkcs11key)",
         "metadata": r"(\.json$|\.sbom$|\.blockmap$)",
     }
