cafefdb7fef09dfb6bb14ad09800f2e3ebe353b3
Daniel Braun <daniel.braun@outlook.co.il>
2023-05-24 11:28:11 +0000

    fix: natsort instead of semver sort (support for non-semver version convention such as date based)

--- nanolayer/installers/gh_release/resolvers/release_resolver.py
+++ nanolayer/installers/gh_release/resolvers/release_resolver.py
@@ -1,17 +1,15 @@
-import json
 import re
-import urllib
 from typing import Any, Dict, List
 
 import invoke
-import semver
+from natsort import natsorted
 
 
 class ReleaseResolver:
     class ReleaseResolverError(Exception):
         pass
 
-    GIT_VERSION_TAG_REGEX = "(?:tags\/)(v)?([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$"
+    GIT_VERSION_TAG_REGEX = "(?:tags\/)(v?[0-9]+\.[0-9]+\.?[0-9]+?)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$"
 
     @classmethod
     def get_version_tags(cls, repo: str) -> List[str]:
@@ -23,41 +21,18 @@ class ReleaseResolver:
                 re.findall(cls.GIT_VERSION_TAG_REGEX, line.strip())
                 for line in response.stdout.split("\n")
             ]
-        return [
-            match[0][0] + ".".join(match[0][1:-1]) + match[0][-1]
-            for match in matches
-            if len(match) == 1
-        ]
+            # merge match tuples into a full version string
+            stringified_matches = [
+                "".join(match[0]) for match in matches if len(match) == 1
+            ]
+            return stringified_matches
+        return []
 
     @classmethod
     def get_latest_stable_version(cls, repo: str) -> List[str]:
         all_version_tags = cls.get_version_tags(repo)
 
-        def strip_prefix(value: str, prefix: str) -> str:
-            if value[: len(prefix)] == prefix:
-                return value[len(prefix) :]
-            return value
-
-        semversions = [
-            semver.VersionInfo.parse(strip_prefix(version, "v"))
-            if semver.VersionInfo.isvalid(strip_prefix(version, "v"))
-            else semver.VersionInfo(0, 0, 0)
-            for version in all_version_tags
-        ]
-
-        sorted_tuples = sorted(
-            zip(semversions, all_version_tags), key=lambda pair: pair[0]
-        )
-
-        stable_semversions = list(
-            filter(
-                lambda version_tuple: version_tuple[0].build is None
-                and version_tuple[0].prerelease is None,
-                sorted_tuples,
-            )
-        )
-
-        return str(stable_semversions[-1][1])
+        return natsorted(all_version_tags)[-1]
 
     @classmethod
     def resolve(cls, asked_version: str, repo: str) -> Dict[str, Any]:
