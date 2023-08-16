2e7a91b37a3f97c99ed8eaef7306c508f22213bd
Daniel Braun <daniel.braun@outlook.co.il>
2023-08-16 15:00:58 +0000

    fix: qemu for arm testing was not installed properly in devcontainer setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,6 +6,7 @@ npm install -g @devcontainers/cli
 
 pip install -e .[dev]
 
-apt-get -y install qemu binfmt-support qemu-user-static
+sudo apt-get update
+sudo apt-get -y install qemu binfmt-support qemu-user-static
 
 docker run --rm --privileged multiarch/qemu-user-static:latest --reset --credential yes -p yes
